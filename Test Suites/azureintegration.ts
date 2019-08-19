e68fe85bf2a17752d255e03d1622b3391edcfb02
tupai <tupai@suvendus-air>
2019-08-19 20:07:01 +0530

    added test suites

---
+++ Test Suites/azureintegration.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>azureintegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6c1f7a6b-7503-4ef1-9b94-4ab34493b33e</testSuiteGuid>
+   <testCaseLink>
+      <guid>a5bb49bd-5ed9-4daa-9b1b-a04eeab46430</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/azuredevopsintegration</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
